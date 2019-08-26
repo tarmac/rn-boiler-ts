42889c2d8747a41da827e8450669a0f9f30e71d7
Ata Sanchez <gahs94@gmail.com>
2019-08-25 22:34:31 -0300

    config(Babel): Add new plugins to import XML and SVG files and add support for decorators.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['babel-plugin-styled-components'],
+  plugins: [
+    [
+      'babel-plugin-inline-import',
+      {
+        extensions: ['.xml', '.svg'],
+      },
+    ],
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    'babel-plugin-styled-components',
+  ],
 };
